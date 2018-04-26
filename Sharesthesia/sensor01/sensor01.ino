a13eadb795a8f9f4e3718e74b0c18551cd062463
three4c <three4c@gmail.com>
2018-04-26 21:27:57 +0900

    コンテンツの追加

---
+++ Sharesthesia/sensor01/sensor01.ino
@@ -0,0 +1,40 @@
+int val1 ;
+int val2 ;
+int val3 ;
+
+// 電源起動時とリセットの時だけのみ処理される(初期化と設定処理)
+void setup() {
+  Serial.begin(9600) ;                    // 9600bpsでシリアル通信のポートを開きます
+}
+// 繰り返し実行される処理(メインの処理)
+void loop() {
+  val1 = analogRead(A0)  ;                     // センサーから読み込む
+  val2 = analogRead(A1)  ;
+  val3 = analogRead(A2)  ;
+
+  
+  if (val1 > 350) {
+    Serial.write(1);
+    delay(500);
+  } else if (val2 > 350) {
+    Serial.write(2);
+    delay(500);
+  } else if (val3 > 350) {
+    Serial.write(3);
+    delay(500);
+  }else {
+    Serial.write(0);
+  }
+ 
+  
+  Serial.print(val1);
+  Serial.print(" ");
+  Serial.print(val2);
+  Serial.print(" ");
+  Serial.println(val3);
+  Serial.println(" ");
+  delay(500);
+  
+  
+}
+
---
+++ Sharesthesia/sensor02/sensor02.ino
@@ -0,0 +1,31 @@
+int val4 ;
+int val5 ;
+
+// 電源起動時とリセットの時だけのみ処理される(初期化と設定処理)
+void setup() {
+  Serial.begin(9600) ;                    // 9600bpsでシリアル通信のポートを開きます
+}
+// 繰り返し実行される処理(メインの処理)
+void loop() {
+  val4 = analogRead(A3)  ;
+  val5 = analogRead(A4)  ;
+
+ 
+ if (val4 > 350) {
+    Serial.write(4);
+    delay(500);
+  } else if (val5 > 350) {
+    Serial.write(5);
+    delay(500);
+  }else {
+    Serial.write(0);
+  }
+  
+ 
+  Serial.print(val4);
+  Serial.print(" ");
+  Serial.println(val5);
+  delay(500);
+  
+}
+
